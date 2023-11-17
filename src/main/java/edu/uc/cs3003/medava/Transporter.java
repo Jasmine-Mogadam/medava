c1ea320f642c3fabe06039a00e2c3101920073a4
Jasmine Mogadam <47947909+Jasmine-Mogadam@users.noreply.github.com>
2023-11-17 18:10:54 -0500

    homework done

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,3 +1,52 @@
 package edu.uc.cs3003.medava;
+import java.util.List;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
 public class Transporter {
+    private String mTransporterName;
+    private List<Shippable> goods;
+    private double mLowTemperature, mHighTemperature;
+
+    public Transporter(String transporterName, double lowTemp, double highTemp) {
+        mTransporterName = transporterName;
+        mLowTemperature = lowTemp;
+        mHighTemperature = highTemp;
+    }
+
+    public String getTransporterName() {
+        return mTransporterName;
+    }
+
+    {
+        goods = new ArrayList<Shippable>();
+    }
+
+    public void ship() {
+        // Do some shipping!
+    }
+
+    public Shippable unload() {
+        return goods.remove(0);
+    }
+    public boolean isEmpty() {
+        return goods.isEmpty();
+    }
+
+    public boolean load(Shippable itemToLoad) {
+        try {
+            Method isTemperatureRangeAcceptableMethod = itemToLoad.getClass().getMethod("isTemperatureRangeAcceptable",
+                    Double.class, Double.class);
+            boolean resultOfMethodCall = (boolean) isTemperatureRangeAcceptableMethod.invoke(itemToLoad,
+                    Double.valueOf(mLowTemperature), Double.valueOf(mHighTemperature));
+            if (resultOfMethodCall) {
+                goods.add(itemToLoad);
+            }
+            return resultOfMethodCall;
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException e) {
+            return false;
+        }
+    }
 }
